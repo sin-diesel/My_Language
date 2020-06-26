4273ad09734c87d43f68fb1ced51d8b3c749b928
Stas_SD <sidelnikov.si@phystech.edu>
2020-06-26 12:26:41 +0300

    Initializing project base

---
+++ cmake-build-debug/CMakeFiles/My_Language.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/My_Language.dir/main.c.o"
+  "My_Language.pdb"
+  "My_Language"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/My_Language.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/My_Language.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for My_Language.
+# This may be replaced when dependencies are built.
